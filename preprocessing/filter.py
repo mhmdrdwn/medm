30dbe76dbfaa076e2aaaa834a8d27532a1bd9d97
Mohamed <mhmdrdwn88@gmail.com>
2021-01-27 07:49:50 +0100

    one file out of core

--- preprocessing/filter.py
+++ preprocessing/filter.py
@@ -1,11 +1,12 @@
 import json
 import gzip
 import io
+from tqdm import tqdm
 
 paper_ids_to_keep = set()
 with gzip.open('20200705v1/full/metadata/metadata_0.jsonl.gz', 'rb') as gz, open('20200705v1/full/metadata/metadata_0_filtered.jsonl', 'wb') as f_out:
     f = io.BufferedReader(gz)
-    for line in f.readlines():
+    for line in tqdm(f.readlines()):
         metadata_dict = json.loads(line)
         paper_id = metadata_dict['paper_id']
         mag_field_of_study = metadata_dict['mag_field_of_study']
@@ -13,13 +14,13 @@ with gzip.open('20200705v1/full/metadata/metadata_0.jsonl.gz', 'rb') as gz, open
             paper_ids_to_keep.add(paper_id)
             f_out.write(line)
 
-
 # now, we get those papers' full text
-with gzip.open(batch['input_pdf_parses_path'], 'rb') as gz, open(batch['output_pdf_parses_path'], 'wb') as f_out:
+with gzip.open('20200705v1/full/pdf_parses/pdf_parses_0.jsonl.gz', 'rb') as gz, open('20200705v1/full/pdf_parses/pdf_parses_0_filtered.jsonl', 'wb') as f_out:
     f = io.BufferedReader(gz)
-    for line in f.readlines():
+    for line in tqdm(f.readlines()):
         metadata_dict = json.loads(line)
         paper_id = metadata_dict['paper_id']
         if paper_id in paper_ids_to_keep:
             f_out.write(line)
+
 
